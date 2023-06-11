710bfc3be51d862d79018dd332b708945c380b48
maomaomeimei <133650461+maomaomeimei@users.noreply.github.com>
2023-06-11 15:07:26 +0200

    Update 0515rwd.js

--- 0515rwd.js
+++ 0515rwd.js
@@ -1,3 +1,14 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+
+// Get the button:
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
